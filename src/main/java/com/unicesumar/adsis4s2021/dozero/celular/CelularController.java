c2092b7def3c8d71a3563d3954fefa0045040f73
ricardotakakura <ricardottakakura@gmail.com>
2021-11-12 21:39:51 -0200

    12,11

--- src/main/java/com/unicesumar/adsis4s2021/dozero/celular/CelularController.java
+++ src/main/java/com/unicesumar/adsis4s2021/dozero/celular/CelularController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.unicesumar.adsis4s2021.dozero.base.BaseCrudController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/celulares")
 public class CelularController extends BaseCrudController<Celular, CelularRepository, CelularService> {
 
 }
